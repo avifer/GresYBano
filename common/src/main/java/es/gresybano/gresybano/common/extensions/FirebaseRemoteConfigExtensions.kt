80e2582bd3bb51d5282ebde6c5e8381bdf630ac6
Francisco Javier Avilés <franciscojavieravifer@gmail.com>
2022-03-05 16:21:51 +0100

    Fix update from Android Base Project

--- common/src/main/java/es/gresybano/gresybano/common/extensions/FirebaseRemoteConfigExtensions.kt
+++ common/src/main/java/es/gresybano/gresybano/common/extensions/FirebaseRemoteConfigExtensions.kt
@@ -2,15 +2,16 @@ package es.gresybano.gresybano.common.extensions
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
-private const val KEY_MIN_VERSION_APP = "min_version_app"
+private const val LAST_VERSION_APP = "last_version_app"
 private const val KEY_MIN_VERSION_APP_DEFAULT = ""
 
 fun FirebaseRemoteConfig.getDefaultValues(): HashMap<String, Any> {
     return hashMapOf(
-        KEY_MIN_VERSION_APP to KEY_MIN_VERSION_APP_DEFAULT,
+        LAST_VERSION_APP to KEY_MIN_VERSION_APP_DEFAULT,
     )
 }
 
-fun FirebaseRemoteConfig.getMinVersion() = getString(KEY_MIN_VERSION_APP)
+fun FirebaseRemoteConfig.getLastVersionApp() = getString(LAST_VERSION_APP)
 
-fun FirebaseRemoteConfig.needForceUpdate(versionName: String) = versionName < getMinVersion()
+fun FirebaseRemoteConfig.isThereAnUpdateAvailable(versionName: String) =
+    versionName < getLastVersionApp()
