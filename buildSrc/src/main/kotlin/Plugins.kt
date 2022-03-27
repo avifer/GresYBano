82baa74629a56b1ce82071a8985d8c74a096c5df
Francisco Javier Avilés <franciscojavieravifer@gmail.com>
2022-03-27 19:21:21 +0200

    Fix activity_host.xml

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -5,8 +5,6 @@ object Plugins {
     const val KOTLIN_KAPT = "kotlin-kapt"
     const val DAGGER_HILT = "dagger.hilt.android.plugin"
     const val JETBRAINS_KOTLIN = "org.jetbrains.kotlin.android"
-    const val JAVA_LIBRARY = "java-library"
-    const val JETBRAINS_KOTLIN_JAVA = "org.jetbrains.kotlin.jvm"
     const val NAVIGATION_SAFEARGS = "androidx.navigation.safeargs.kotlin"
     const val GMS_GOOGLE_SERVICES = "com.google.gms.google-services"
     const val FIREBASE_CRASHLYTICS = "com.google.firebase.crashlytics"
