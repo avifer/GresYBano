f1081d9f3276616ecbdff945d9a3ff4bf272a795
Avifer <franciscojavieravifer@gmail.com>
2022-08-04 18:19:28 +0200

    GRES-11 Refactor save category to favorite

--- feature/onboarding/src/main/java/es/gresybano/gresybano/feature/onboarding/ui/viewmodel/OnBoardingViewModel.kt
+++ feature/onboarding/src/main/java/es/gresybano/gresybano/feature/onboarding/ui/viewmodel/OnBoardingViewModel.kt
@@ -2,11 +2,10 @@ package es.gresybano.gresybano.feature.onboarding.ui.viewmodel
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import es.gresybano.gresybano.common.usecase.SubscribeToTagsUseCase
 import es.gresybano.gresybano.common.util.PreferencesUtil
 import es.gresybano.gresybano.common.viewmodel.BaseViewModel
 import es.gresybano.gresybano.domain.category.entity.CategoryBo
-import es.gresybano.gresybano.feature.onboarding.domain.SaveCategoriesLocal
+import es.gresybano.gresybano.common.usecase.AddCategoriesToFavoriteUseCase
 import es.gresybano.gresybano.feature.onboarding.ui.view.fragment.OnBoardingFragmentDirections
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -14,15 +13,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
-    private val saveCategoriesLocal: SaveCategoriesLocal,
-    private val subscribeToTagsUseCase: SubscribeToTagsUseCase,
+    private val saveCategoriesLocal: AddCategoriesToFavoriteUseCase,
     private val preferencesUtil: PreferencesUtil
 ) : BaseViewModel() {
 
     fun saveFavoriteCategories(list: List<CategoryBo>) {
         viewModelScope.launch {
             saveCategoriesLocal(list).collect {
-                subscribeToTagsUseCase(*list.mapNotNull { it.tag }.toTypedArray())
                 onboardFinished()
                 navigateToHome()
             }
