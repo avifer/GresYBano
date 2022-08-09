cc1844a9c4159b7fadb5ce3dc6443b6fe155aa54
Avifer <franciscojavieravifer@gmail.com>
2022-08-09 17:13:33 +0200

    GRES-13 Implement action share app

--- feature/application/src/main/java/es/gresybano/gresybano/feature/application/viewmodel/SettingsViewModel.kt
+++ feature/application/src/main/java/es/gresybano/gresybano/feature/application/viewmodel/SettingsViewModel.kt
@@ -9,13 +9,23 @@ import es.gresybano.gresybano.feature.application.view.adapter.ElementSettingsAd
 
 class SettingsViewModel : BaseViewModel() {
 
+    lateinit var contextToString: (() -> Context)
+
+    private var actionShareApp: (() -> Unit)? = null
+
+    fun saveActionShareApp(actionShareApp: (() -> Unit)) {
+        this.actionShareApp = actionShareApp
+    }
+
     fun actionClickInElement(elementClicked: ElementSettingVo) {
         when (elementClicked) {
             is ElementSettingVo.ElementVo -> {
                 when (elementClicked.id) {
                     IdElementSetting.NOTIFICATIONS -> {}    //TODO Navegar hacia la pantalla de configuracion de notificaciones
-                    IdElementSetting.RANK_US -> {}          //TODO Navegar hacia google play para valorar la app
-                    IdElementSetting.SHARE_APP -> {}        //TODO Lanzar intent para compartir la app
+                    IdElementSetting.RATE_US -> {}          //TODO Navegar hacia google play para valorar la app
+                    IdElementSetting.SHARE_APP -> {
+                        actionShareApp?.invoke()
+                    }
                     IdElementSetting.HELP -> {}             //TODO Navegar hacia la pantalla de ayuda
                     IdElementSetting.VERSION -> {}          //TODO Implementar pantalla de versión
                 }
@@ -26,47 +36,40 @@ class SettingsViewModel : BaseViewModel() {
         }
     }
 
-    fun getListElementSettings(context: Context, version: String) =
+    fun getListElementSettings(version: String) =
         listOf(
             ElementSettingVo.ElementVo(
                 id = IdElementSetting.NOTIFICATIONS,
                 icon = es.gresybano.gresybano.common.R.drawable.icon_notification_off,
-                name = getStringVM(context, R.string.element_notification_settings__title),
-                description = getStringVM(
-                    context,
-                    R.string.element_notification_settings__description
-                ),
+                name = getStringVM(R.string.element_notification_settings__title),
+                description = getStringVM(R.string.element_notification_settings__description),
             ),
             ElementSettingVo.ElementVo(
-                id = IdElementSetting.RANK_US,
+                id = IdElementSetting.RATE_US,
                 icon = es.gresybano.gresybano.common.R.drawable.icon_star,
-                name = getStringVM(context, R.string.element_rank_us_settings__title),
-                description = getStringVM(context, R.string.element_rank_us_settings__description),
+                name = getStringVM(R.string.element_rate_us_settings__title),
+                description = getStringVM(R.string.element_rate_us_settings__description),
             ),
             ElementSettingVo.ElementVo(
                 id = IdElementSetting.SHARE_APP,
                 icon = es.gresybano.gresybano.common.R.drawable.icon_share,
-                name = getStringVM(context, R.string.element_share_app_settings__title),
-                description = getStringVM(
-                    context,
-                    R.string.element_share_app_settings__description
-                ),
+                name = getStringVM(R.string.element_share_app_settings__title),
+                description = getStringVM(R.string.element_share_app_settings__description),
             ),
             ElementSettingVo.ElementVo(
                 id = IdElementSetting.HELP,
                 icon = es.gresybano.gresybano.common.R.drawable.icon_help,
-                name = getStringVM(context, R.string.element_help_settings__title),
-                description = getStringVM(context, R.string.element_help_settings__description),
+                name = getStringVM(R.string.element_help_settings__title),
+                description = getStringVM(R.string.element_help_settings__description),
             ),
             ElementSettingVo.SeparatorVo,
             ElementSettingVo.ElementVo(
                 id = IdElementSetting.VERSION,
-                name = getStringVM(context, R.string.element_version_settings__name),
+                name = getStringVM(R.string.element_version_settings__name),
                 description = version,
             ),
         )
 
-    private fun getStringVM(context: Context, @StringRes idString: Int) =
-        context.getString(idString)
+    private fun getStringVM(@StringRes idString: Int) = contextToString().getString(idString)
 
 }
