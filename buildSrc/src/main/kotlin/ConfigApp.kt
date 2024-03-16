9f4ea49879124778784416b39c2770f746fe8c77
avifer <franciscojavieravifer@gmail.com>
2024-03-16 16:10:38 +0100

    Update target sdk

--- buildSrc/src/main/kotlin/ConfigApp.kt
+++ buildSrc/src/main/kotlin/ConfigApp.kt
@@ -1,8 +1,8 @@
 object ConfigApp {
     const val APPLICATION_ID = "es.gresybano.gresybano"
     const val MIN_SDK = 23
-    const val TARGET_SDK = 31
-    const val COMPILE_SDK = 31
-    const val BUILD_TOOLS_VERSION = "31.0.0"
-    const val VERSION_NAME = "1.0.1"
+    const val TARGET_SDK = 34
+    const val COMPILE_SDK = 34
+    const val BUILD_TOOLS_VERSION = "34.0.0"
+    const val VERSION_NAME = "1.0.2"
 }
